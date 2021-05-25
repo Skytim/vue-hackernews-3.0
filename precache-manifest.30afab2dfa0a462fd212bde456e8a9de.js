f10aaf38db6576eb660f2e87f257e0c594a98834
Tim Tsai <sky22357168@gmail.com>
2021-05-25 15:08:56 +0800

    deploy

---
+++ precache-manifest.30afab2dfa0a462fd212bde456e8a9de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b633703668a242432e5c",
+    "url": "/vue-hackernews-3.0/css/app.f7d95841.css"
+  },
+  {
+    "revision": "99fcd324fc5250ebf02c",
+    "url": "/vue-hackernews-3.0/css/chunk-15b465c2.b97ec903.css"
+  },
+  {
+    "revision": "043da266556a49207532",
+    "url": "/vue-hackernews-3.0/css/chunk-vendors.0f1ada3b.css"
+  },
+  {
+    "revision": "d34a6356569a29137e10e972a9910b36",
+    "url": "/vue-hackernews-3.0/index.html"
+  },
+  {
+    "revision": "b633703668a242432e5c",
+    "url": "/vue-hackernews-3.0/js/app.3cbc629a.js"
+  },
+  {
+    "revision": "c7e8f1b9eadd0ee0b7fd",
+    "url": "/vue-hackernews-3.0/js/chunk-028ca85d.f8a4a2cb.js"
+  },
+  {
+    "revision": "99fcd324fc5250ebf02c",
+    "url": "/vue-hackernews-3.0/js/chunk-15b465c2.15a8aa0c.js"
+  },
+  {
+    "revision": "20d50a94c93c3764c8d1",
+    "url": "/vue-hackernews-3.0/js/chunk-2d0b9824.71a6a39a.js"
+  },
+  {
+    "revision": "043da266556a49207532",
+    "url": "/vue-hackernews-3.0/js/chunk-vendors.65995656.js"
+  },
+  {
+    "revision": "c69767641f9b70e04ae334840cc678b2",
+    "url": "/vue-hackernews-3.0/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-hackernews-3.0/robots.txt"
+  }
+]);
